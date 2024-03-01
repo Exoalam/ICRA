5dd19d396139660a8795e2a9079dd8114b955740
Exoalam <nafiul.arpon@gmail.com>
2024-03-02 05:16:58 +0600

    update

--- final_model.py
+++ final_model.py
@@ -6,39 +6,39 @@ from ultralytics.utils.plotting import Annotator
 import pyrealsense2 as rs
 from realsense_depth import *
 from scipy.spatial import distance
-from keras.applications import VGG19
-from keras.layers import Dense, Flatten, Input,Concatenate
-from keras.models import Model,load_model, Sequential
-from keras.preprocessing import image as keras_image
-from keras.applications.vgg19 import preprocess_input
+# from keras.applications import VGG19
+# from keras.layers import Dense, Flatten, Input,Concatenate
+# from keras.models import Model,load_model, Sequential
+# from keras.preprocessing import image as keras_image
+# from keras.applications.vgg19 import preprocess_input
 
 
-def orientation(image):
-    resized_image = cv2.resize(image, (224, 224))  
-    img_array = keras_image.img_to_array(resized_image)
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = preprocess_input(img_array)
-    base_model = VGG19(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-    for layer in base_model.layers:
-        layer.trainable = False
-    x = Flatten()(base_model.output)
-    x = Dense(512, activation='relu')(x)
-    x = Dense(256, activation='relu')(x)
-    output_layer = Dense(3, activation='linear')(x) 
-    model = Model(inputs=base_model.input, outputs=output_layer)
-    model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
-    model.load_weights('vgg19_1.h5')
-    return model.predict(img_array)
+# def orientation(image):
+#     resized_image = cv2.resize(image, (224, 224))  
+#     img_array = keras_image.img_to_array(resized_image)
+#     img_array = np.expand_dims(img_array, axis=0)
+#     img_array = preprocess_input(img_array)
+#     base_model = VGG19(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+#     for layer in base_model.layers:
+#         layer.trainable = False
+#     x = Flatten()(base_model.output)
+#     x = Dense(512, activation='relu')(x)
+#     x = Dense(256, activation='relu')(x)
+#     output_layer = Dense(3, activation='linear')(x) 
+#     model = Model(inputs=base_model.input, outputs=output_layer)
+#     model.compile(optimizer='adam', loss='mean_squared_error', metrics=['mae'])
+#     model.load_weights('vgg19_1.h5')
+#     return model.predict(img_array)
 
-def hybride_reg(x1):
-    model = Sequential()
-    model.add(Dense(32, input_dim=1, kernel_initializer='normal', activation='relu'))  
-    model.add(Dense(16, activation='relu'))
-    model.add(Dense(2, activation='linear'))
-    model.compile(loss='mean_squared_error', optimizer='adam')
-    model.load_weights('best_model.h5')
-    xd1 = np.array([[x1]])
-    return model.predict([xd1])
+# def hybride_reg(x1):
+#     model = Sequential()
+#     model.add(Dense(32, input_dim=1, kernel_initializer='normal', activation='relu'))  
+#     model.add(Dense(16, activation='relu'))
+#     model.add(Dense(2, activation='linear'))
+#     model.compile(loss='mean_squared_error', optimizer='adam')
+#     model.load_weights('best_model.h5')
+#     xd1 = np.array([[x1]])
+#     return model.predict([xd1])
 
 def Remapping(x2,x1):
     print([x2[0] - x1[0],x2[1] - x1[1],np.abs(x2[2] - x1[2])])
@@ -78,6 +78,8 @@ model = YOLO('yolov8n.pt')
 dc = DepthCamera()
 hit_map = np.zeros((1000,1000))
 detect_list = [39]
+height  = 0
+width = 0
 while True:
     
     ret, depth_frame, color_frame, depth_info = dc.get_frame()
@@ -102,7 +104,6 @@ while True:
             cropped = color_frame[top_left[1] : bottom_right[1], top_left[0] : bottom_right[0]]
             if c in detect_list:
                 #print(orientation(cropped))
-                print(point)
                 points = dc.Global_points(point[0],point[1])
                 dx = 500+points[0][0]*100
                 dy = 500+points[0][1]*100
@@ -115,7 +116,8 @@ while True:
     cv2.imshow('Vision', color_frame)    
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
-        ori = orientation(cropped)
+        #ori = orientation(cropped)
+        depth = points[0][2]
         break  
 
 
@@ -124,29 +126,31 @@ indices = np.where(map['hit'] > 1)
 points = list(zip(indices[0], indices[1], indices[2]))
 points = max_hit(points)
 coordinate = points[0]
+width = w/coordinate[2]*points[0][2]
+height = h/coordinate[2]*points[0][2]
 
+print(width, height)
 #2nd Camera
 
 ret, depth_frame, color_frame, depth_info = dc.get_frame()
 x1 = float(coordinate[0])-500
 x2 = float(coordinate[1])-500
-print(x1,x2)
 coor = Remapping([x1,x2,coordinate[2]],[0,0,0])
-rotation = Remapping([ori[0][0],ori[0][1],ori[0][2]],[0,0,0])
+#rotation = Remapping([ori[0][0],ori[0][1],ori[0][2]],[0,0,0])
 points = reverse(depth_info,coor[0],coor[1],coor[2])
 distance = np.sqrt(np.power(coor[0],2)+np.power(coor[1],2)+np.power(coor[2],2))
-hw = hybride_reg(distance/100)
-cv2.circle(color_frame, (round(points[0]),round(points[1])), 4, (0, 0, 255))
-h = round(np.abs(hw[0][0]))
-w = h*.33
+# #hw = hybride_reg(distance/100)
+# cv2.circle(color_frame, (round(points[0]),round(points[1])), 4, (0, 0, 255))
+# h = round(np.abs(hw[0][0]))
+# w = h*.33
 
 x1 = round(points[0])
 y1 = round(points[1])
-print(x1,y1)
-x = int(x1 - w/2)
-y = int(y1 - h/2)
-x2 = int(x1 + w/2)
-y2 = int(y1 + h/2)
+
+x = int(x1 - width/2)
+y = int(y1 - height/2)
+x2 = int(x1 + width/2)
+y2 = int(y1 + height/2)
 cv2.rectangle(color_frame, (x, y), (x2, y2), (0, 255, 0), 2)
 cv2.imshow('Vision', color_frame)     
 cv2.waitKey(0)
